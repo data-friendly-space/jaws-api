425421ac8647bc037f71ed521f6789bb69ec5edd
luciano_assefh <luciano.assefh@stoneridge.com>
2024-11-19 14:44:52 -0300

    first commit - setting up

--- user_management/interfaces/controllers/get_users_controller.py
+++ user_management/interfaces/controllers/get_users_controller.py
@@ -1,7 +1,7 @@
 from rest_framework import status
 from rest_framework.decorators import api_view
 
-from user_management.interfaces.controllers.helpers import api_response
+from user_management.interfaces.controllers.helpers.api_response import api_response
 
 
 @api_view(['GET'])
