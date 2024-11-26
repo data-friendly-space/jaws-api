1177a2aca3d893af6f8f57834b1d0071b85e1187
Gaston Mastrapasqua <gastimastra@gmail.com>
2024-11-27 00:11:45 +0100

    refactor: remove unused url

--- analysis/urls.py
+++ analysis/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from analysis.interfaces.controllers.create_analysis_controller import create_analysis_controller
 from analysis.interfaces.controllers.get_analysis_by_id_controller import get_analysis_by_id_controller
 from analysis.interfaces.controllers.get_analysis_controller import get_analysis_controller
-from analysis.interfaces.controllers.analysis_scope_controller import get_analysis_scope_controller
 from analysis.interfaces.controllers.put_analysis_scope_controller import put_analysis_scope_controller
 
 urlpatterns = [
@@ -11,5 +10,4 @@ urlpatterns = [
     path("create", create_analysis_controller, name="create_analysis"),
     path("<slug:id>", get_analysis_by_id_controller, name="get_analysis"),
     path("<slug:id>/update", put_analysis_scope_controller, name="put_analysis_scope"),
-    path("<slug:id>/scope", get_analysis_scope_controller, name="get_analysis_scope"),
 ]
