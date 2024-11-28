5b48f1081ea2c9b42424098021047e474169449f
Gaston Mastrapasqua <gastimastra@gmail.com>
2024-11-28 10:39:14 +0100

    fix: wrong relationships

--- user_management/interfaces/serializers/user_serializer.py
+++ user_management/interfaces/serializers/user_serializer.py
@@ -1,23 +1,27 @@
 from rest_framework import serializers
 
+from user_management.interfaces.serializers.affiliation_serializer import AffiliationTO
+from user_management.interfaces.serializers.organization_serializer import OrganizationTO
+from user_management.interfaces.serializers.position_serializer import PositionTO
 from user_management.models import User
 
 
 class UserSerializer(serializers.Serializer):
+    id = serializers.CharField(max_length=36)
     name = serializers.CharField()
     lastname = serializers.CharField()
     email = serializers.EmailField()
     country = serializers.CharField()
-    position = serializers.JSONField()
-    affiliation = serializers.JSONField()
-    organization = serializers.JSONField()
+    position = PositionTO()
+    affiliation = AffiliationTO()
+    organization = OrganizationTO()
     uiConfiguration = serializers.JSONField()
     profileImage = serializers.CharField(required=False, allow_null=True)
 
     class Meta:
         model = User
         fields = [
-            'name', 'lastname', 'email', 'country',
+            'id', 'name', 'lastname', 'email', 'country',
             'profileImage', 'position', 'affiliation',
             'organization', 'uiConfiguration'
         ]
