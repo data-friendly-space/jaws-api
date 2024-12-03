bf0530f432954925f32bcbeaefdb96f5b7924773
luciano_assefh <luciano.assefh@stoneridge.com>
2024-12-03 17:44:22 -0300

    JAWS-271

--- user_management/models/organization.py
+++ user_management/models/organization.py
@@ -7,7 +7,7 @@ class Organization(models.Model):
     name = models.CharField(max_length=100)
 
     def __str__(self):
-        return self.name
+        return str(self.name)
 
     class Meta:
         db_table = 'organization'
