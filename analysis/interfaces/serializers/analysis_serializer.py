a6ff9368392a28571db0a6a72872fe5ab72a77d8
José Jaramillo <jjosejaramillos@gmail.com>
2024-12-03 15:24:49 -0300

    feat: add linter and improve code (#7)

--- analysis/interfaces/serializers/analysis_serializer.py
+++ analysis/interfaces/serializers/analysis_serializer.py
@@ -1,3 +1,4 @@
+'''This module contains the analysis serializer'''
 from rest_framework import serializers
 from analysis.models.analysis import Analysis
 from app.core.serializer.CamelCaseMixin import CamelCaseMixin
--- analysis/interfaces/serializers/sector_serializer.py
+++ analysis/interfaces/serializers/sector_serializer.py
@@ -1,3 +1,4 @@
+'''This module contains the sector serializer'''
 from rest_framework import serializers
 
 from analysis.models.sector import Sector
@@ -9,5 +10,6 @@ class SectorSerializer(serializers.ModelSerializer):
     id = serializers.CharField(max_length=36, read_only=True)
 
     class Meta:
+        '''Base class'''
         model = Sector
         fields = ['id']
