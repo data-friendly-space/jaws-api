55bdcbeec446a8d30ebeafb1fc7cfc6db1c65b86
Gaston Mastrapasqua <gastimastra@gmail.com>
2024-12-07 18:43:57 +0100

    feat: add endpoint to load administrative divisions

--- analysis/urls.py
+++ analysis/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 
+from analysis.command.load_administrative_divisions import load_administrative_divisions
 from analysis.interfaces.controllers.create_analysis_controller import create_analysis_controller
 from analysis.interfaces.controllers.get_analysis_by_id_controller import get_analysis_by_id_controller
 from analysis.interfaces.controllers.get_analysis_controller import get_analysis_controller
@@ -8,6 +9,7 @@ from analysis.interfaces.controllers.put_analysis_scope_controller import put_an
 urlpatterns = [
     path("", get_analysis_controller, name="get_analyses"),
     path("create", create_analysis_controller, name="create_analysis"),
+    path("load-administrative-divisions", load_administrative_divisions, name="load_administrative_divisions"),
     path("<slug:id>", get_analysis_by_id_controller, name="get_analysis"),
-    path("<slug:id>/update", put_analysis_scope_controller, name="put_analysis_scope"),
+    path("<slug:id>/update", put_analysis_scope_controller, name="put_analysis_scope")
 ]
