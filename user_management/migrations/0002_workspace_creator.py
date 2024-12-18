2db028e17399739ceac2b4f8f7005bb6beed7c5a
Gaston Mastrapasqua <gastimastra@gmail.com>
2024-12-18 17:56:35 +0100

    fix: migrations using unexisting initial data

--- user_management/migrations/0002_workspace_creator.py
+++ user_management/migrations/0002_workspace_creator.py
@@ -4,6 +4,8 @@ import django.db.models.deletion
 from django.conf import settings
 from django.db import migrations, models
 
+from user_management.models.user import User
+
 
 class Migration(migrations.Migration):
 
@@ -15,7 +17,10 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='workspace',
             name='creator',
-            field=models.ForeignKey(default='4e469429-5be2-4033-bcac-1275cd44ac63', on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL),
+            field=models.ForeignKey(
+                default=User.objects.first().id,
+                on_delete=django.db.models.deletion.CASCADE,
+                to=settings.AUTH_USER_MODEL),
             preserve_default=False,
         ),
     ]
