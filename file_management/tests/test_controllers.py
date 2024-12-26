e4ca05d8ab3c18108556082689339e3dacad1146
Gaston Mastrapasqua <gastimastra@gmail.com>
2024-12-26 16:00:11 +0100

    refactor: isolate controller test

--- file_management/tests/test_controllers.py
+++ file_management/tests/test_controllers.py
@@ -1,12 +1,10 @@
 """This module contains the tests for the controllers"""
 
+from unittest.mock import MagicMock, patch
 from django.test import TestCase
 from django.urls import reverse
 
-from analysis.models.analysis import Analysis
 from common.test_utils import create_logged_in_client
-from user_management.models.organization import Organization
-from user_management.models.workspace import Workspace
 
 
 class TestCreatePresignedUrlFileUploadController(TestCase):
@@ -14,54 +12,48 @@ class TestCreatePresignedUrlFileUploadController(TestCase):
 
     def setUp(self):
         self.client, self.user = create_logged_in_client()
-        self.org = Organization.objects.create(name="TestOrganization2")
-        self.workspace = Workspace.objects.create(
-            title="TestWorkspace1",
-            organization=self.org,
-            facilitator_id=self.user.id,
-            creator_id=self.user.id,
-        )
-        self.test_analysis = Analysis.objects.create(
-            title="TestAnalysis1",
-            workspace_id=self.workspace.id,
-            end_date="2024-12-17",
-            creator_id=self.user.id,
-        )
         self.url = reverse("get_upload_file_url")
 
-    def test_valid_data(self):
+    @patch(
+        "file_management.interfaces.controllers.create_presigned_url_upload_file_controller.FileManagementServiceImpl"
+    )
+    def test_valid_data(self, mock_service):
         """Test if creating a presigned url with valid data works"""
-        valid_data = {"filename": "Prueba", "analysisId": 15}
+        mock_service_instance = MagicMock()
+        mock_service.return_value = mock_service_instance
+        mock_service_instance.create_presigned_url_upload_file.return_value = {
+            "url": "https://example.com/upload"
+        }
+        valid_data = {"filename": "test.csv", "analysisId": 1}
         response = self.client.post(self.url, valid_data)
         self.assertEqual(response.status_code, 200)
-        self.assertIn(
-            "url",
-            response.data["payload"],
-            "The payload must contain 'url' field"
+        self.assertEqual(
+            response.data["payload"], {"url": "https://example.com/upload"}
         )
-        self.assertIn(
-            "fields",
-            response.data["payload"],
-            "The payload must contain 'fields' field",
-        )
-        self.assertIn(
-            "AWSAccessKeyId",
-            response.data["payload"]["fields"],
-            "The 'fields' field must contain 'AWSAccessKeyId' field",
-        )
-        self.assertIn(
-            "policy",
-            response.data["payload"]["fields"],
-            "The 'fields' field must contain 'policy' field"
-        )
-        self.assertIn(
-            "signature",
-            response.data["payload"]["fields"],
-            "The 'fields' field must contain 'signature' field"
+        mock_service_instance.create_presigned_url_upload_file.assert_called_once_with(
+            self.user, "test.csv", 1
         )
 
-    def test_invalid_data(self):
-        """Test if creating a presigned url with invalid data fails"""
-        invalid_data = {"filename": "Prueba", "asd": 15}
+    def test_missing_fields(self):
+        """Test that calling the api with missing fields fails"""
+        invalid_data = {"filename": "test.csv"}
+
         response = self.client.post(self.url, invalid_data)
+
         self.assertEqual(response.status_code, 400)
+        self.assertIn("All fields are required", response.data["message"])
+
+    @patch(
+        "file_management.interfaces.controllers.create_presigned_url_upload_file_controller.FileManagementServiceImpl"
+    )
+    def test_invalid_data(self, mock_service):
+        """Test if creating a presigned url with invalid data fails"""
+        mock_service_instance = MagicMock()
+        mock_service.return_value = mock_service_instance
+        mock_service_instance.create_presigned_url_upload_file.side_effect = Exception()
+
+        valid_data = {"filename": "test.csv", "analysisId": 1}
+
+        response = self.client.post(self.url, valid_data)
+
+        self.assertEqual(response.status_code, 500)
