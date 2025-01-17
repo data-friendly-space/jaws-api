a8ba0a291067acc09ae89a4db1ff7a0ca127bb6f
Mastra <85653531+gastonmastra@users.noreply.github.com>
2025-01-17 18:03:26 +0100

    feat: get charts (#43)
    * feat: save and retrieve column subpillar
    
    * feat: save chart subpillar
    
    * feat: get charts
    
    * fix: typo in url name

--- charts/services/chart_service.py
+++ charts/services/chart_service.py
@@ -1,6 +1,7 @@
 """Contains the abstract class of chart service"""
 
 from abc import abstractmethod
+from typing import List
 from common.service.base_service import BaseService
 
 
@@ -10,3 +11,13 @@ class ChartService(BaseService):
     @abstractmethod
     def save_chart(self, user, config: dict) -> dict:
         """Save a chart"""
+
+    @abstractmethod
+    def get_charts(self, user, analysis_id: int, subpillars: List[int]) -> List[dict]:
+        """Get the charts of an analysis
+        
+        Keyword arguments:
+        analysis_id -- The id of the analysis
+        subpillars -- the subpillars to use as filter
+        Return: A list of charts as dict
+        """
