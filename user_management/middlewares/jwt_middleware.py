7fbd93ff9829e6127d64f900f5babee82e2bb586
Gaston Mastrapasqua <gastimastra@gmail.com>
2025-01-22 12:28:57 +0100

    fix: health check using jwt

--- user_management/middlewares/jwt_middleware.py
+++ user_management/middlewares/jwt_middleware.py
@@ -15,6 +15,7 @@ class JWTMiddleware:
         excluded_paths = [
             '/auth/complete/google-oauth2/',
             '/auth/login/google-oauth2/',
+            '/jaws-api/health',
             '/jaws-api/user-management/sign-in-with-access-token',
             '/jaws-api/csrf', 
             '/jaws-api/user-management/sign-up',
