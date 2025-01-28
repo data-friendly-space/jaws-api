52bdcae34b26c50763bd8823391422d89063e006
Gaston Mastrapasqua <gastimastra@gmail.com>
2025-01-28 19:25:00 +0100

    refactor: remove comments

--- terraform/modules/s3/datasets/main.tf
+++ terraform/modules/s3/datasets/main.tf
@@ -7,19 +7,6 @@ resource "aws_s3_bucket" "jaws-datasets-s3" {
   })
 }
 
-# resource "aws_s3_bucket_public_access_block" "jaws-datasets-s3-public-access-block" {
-#   bucket = aws_s3_bucket.jaws-datasets-s3.id
-
-#   block_public_acls       = false
-#   block_public_policy     = false
-#   ignore_public_acls      = false
-#   restrict_public_buckets = false
-# }
-
-# resource "aws_s3_bucket_acl" "jaws-datasets-s3-acl" {
-#   bucket = aws_s3_bucket.jaws-datasets-s3.id
-#   acl    = "public-read"
-# }
 resource "aws_s3_bucket_policy" "frontend" {
   bucket = aws_s3_bucket.jaws-datasets-s3.id
 
