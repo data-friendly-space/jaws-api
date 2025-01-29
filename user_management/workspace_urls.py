3e4e3040c41fb616fe7e4a9541230a25a9df6ac2
Gaston Mastrapasqua <gastimastra@gmail.com>
2025-01-29 19:28:37 +0100

    refactor: wrong filenames

---
+++ user_management/workspace_urls.py
@@ -0,0 +1,27 @@
+"""User management URL Configuration"""
+
+from django.urls import path
+
+from analysis.interfaces.controllers.get_analysis_controller import (
+    get_analysis_controller,
+)
+from user_management.interfaces.controllers.create_workspace_controller import (
+    create_workspace_controller,
+)
+from user_management.interfaces.controllers.get_workspace_by_id import (
+    get_workspaces_by_id_controller,
+)
+
+urlpatterns = [
+    path(
+        "<str:workspace_id>",
+        get_workspaces_by_id_controller,
+        name="workspace_by_id",
+    ),
+    path("create", create_workspace_controller, name="create_workspace"),
+    path(
+        "<str:workspace_id>/analyses",
+        get_analysis_controller,
+        name="get_analysis_controller",
+    ),
+]
